90b8357feaf30cfd151a5c25a7132424073e7190
Devin Slauenwhite <devin.slauenwhite@gmail.com>
2023-10-26 15:43:27 +0000

    fix: add customer option back to report

--- erpnext/selling/report/sales_analytics/sales_analytics.js
+++ erpnext/selling/report/sales_analytics/sales_analytics.js
@@ -8,7 +8,7 @@ frappe.query_reports["Sales Analytics"] = {
 			fieldname: "tree_type",
 			label: __("Tree Type"),
 			fieldtype: "Select",
-			options: ["Warehouse"],
+			options: ["Customer", "Warehouse"],
 			default: "Warehouse",
 			reqd: 1
 		},
