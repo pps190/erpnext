288ae174878c131e0a52990167373a9d095838b1
Devin Slauenwhite <devin.slauenwhite@gmail.com>
2024-03-11 22:04:23 -0400

    chore(leo): sort by bill_date

--- erpnext/accounts/report/accounts_payable/accounts_payable.py
+++ erpnext/accounts/report/accounts_payable/accounts_payable.py
@@ -3,11 +3,18 @@
 
 
 from erpnext.accounts.report.accounts_receivable.accounts_receivable import ReceivablePayableReport
-
+from frappe.utils import nowdate
 
 def execute(filters=None):
 	args = {
 		"account_type": "Payable",
 		"naming_by": ["Buying Settings", "supp_master_name"],
 	}
-	return ReceivablePayableReport(filters).run(args)
+	r = ReceivablePayableReport(filters).run(args)
+
+	for d in r[1]:
+		if "bill_date" not in d or not d["bill_date"]:
+			d["bill_date"] = d["posting_date"]
+
+	r[1].sort(key=lambda b: b["bill_date"])
+	return r
