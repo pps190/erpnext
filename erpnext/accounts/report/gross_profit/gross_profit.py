33bf65af2bbfd466a9ba5df5446150f64eed02d8
Devin Slauenwhite <devin.slauenwhite@gmail.com>
2025-02-03 14:08:19 -0500

    fix: sum gross profit amounts when grouped by invoice

--- erpnext/accounts/report/gross_profit/gross_profit.py
+++ erpnext/accounts/report/gross_profit/gross_profit.py
@@ -423,6 +423,8 @@ class GrossProfitGenerator(object):
 
 		if grouped_by_invoice:
 			buying_amount = 0
+			qty = 0
+			base_net_rate = 0
 
 		for row in reversed(self.si_list):
 			if self.filters.get("group_by") == "Monthly":
@@ -466,9 +468,15 @@ class GrossProfitGenerator(object):
 			if grouped_by_invoice:
 				if row.indent == 1.0:
 					buying_amount += row.buying_amount
+					qty += row.qty
+					base_net_rate += row.base_net_rate
 				elif row.indent == 0.0:
 					row.buying_amount = buying_amount
+					row.qty = qty
+					row.base_net_rate = base_net_rate
 					buying_amount = 0
+					qty = 0
+					base_net_rate = 0
 
 			# get buying rate
 			if flt(row.qty):
